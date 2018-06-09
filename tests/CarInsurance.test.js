c8a7102bc74512932f93f389043fc21ed892434d
Juorder Gonzalez quiñonez <>
2018-06-09 02:22:18 -0400

    add car insurance class for handle products

--- tests/CarInsurance.test.js
+++ tests/CarInsurance.test.js
@@ -1,9 +1,11 @@
 const chai = require('chai');
 const { expect } = chai;
 // import CarInsurance class
+const CarInsurance = require('../classes/CarInsurance');
 
 describe('Suite Tests CarInsurance', () => {
-    it('should fail', () => {
-        expect('should fails').to.equal('should fails');
+    it('should instance a valid CarInsurance', () => {
+        const Car = new CarInsurance([]);
+        expect(Car).to.be.an.instanceof(CarInsurance);
     });
 })
